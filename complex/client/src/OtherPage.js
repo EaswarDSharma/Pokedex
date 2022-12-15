98ce4d94176bcfa1dac0fb7cbb2f5d0c3f1d8e22
EaswarDSharma <easwardsharma@gmail.com>
2022-12-15 13:51:13 +0530

    updated export to clear warning

--- complex/client/src/OtherPage.js
+++ complex/client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const page = ()=> {
   return (
     <div>
       Im some other page!
@@ -9,3 +9,4 @@ export default () => {
     </div>
   );
 };
+export default page
