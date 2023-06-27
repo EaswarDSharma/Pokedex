4f4d212b5d8807735c41d00ac7a7b52ace8909af
EaswarDSharma <easwardsharma@gmail.com>
2023-06-27 19:33:39 +0530

    prettiered

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
-import { Typography,IconButton,Button,Paper } from '@mui/material';
-import CatchingPokemonTwoToneIcon from '@mui/icons-material/CatchingPokemonTwoTone';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Grid from '@mui/material/Unstable_Grid2';
-import { makeStyles } from '@mui/styles';
-import pokemon from './pokemon.jpg';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+import { Typography, IconButton, Button, Paper } from "@mui/material";
+import CatchingPokemonTwoToneIcon from "@mui/icons-material/CatchingPokemonTwoTone";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Unstable_Grid2";
+import { makeStyles } from "@mui/styles";
+import pokemon from "./pokemon.jpg";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link to="/">website</Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      {"Copyright © "}
+      <Link to="/">website</Link> {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
@@ -26,66 +25,74 @@ function App() {
   const useStyles = makeStyles({
     root: {
       background: `url(${pokemon}) no-repeat center center fixed`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      width: '100vw',
-      minHeight: '100vh',
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      width: "100vw",
+      minHeight: "100vh",
     },
   });
   const classes = useStyles();
-  return (<div className={classes.root}>
-    <Router>
-      <div className="App">
-        <header >
-        <Grid container  sx={{ flexGrow: 1 }}>
-          <AppBar position="fixed"  style={{ background: 'transparent', boxShadow: 'none'}}>
-            <Toolbar >
-            <Grid xs={6} xsOffset={3} mb={2} mdOffset={0}>
-           <IconButton 
-          aria-label="add to shopping cart" 
-          edge="start"
-          href="/"
-          style={{
-            backgroundColor: 'white',
-            borderRadius: '50%',
-            width: '36px',
-            height: '36px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          >
-          <CatchingPokemonTwoToneIcon  fontSize="large"  htmlColor="#E3242B" />
-          </IconButton>
-           </Grid>
-          <div style={{ flexGrow: 1 }}></div>
-          <IconButton 
-          aria-label="add to shopping cart" 
-          edge="start"
-          href="/otherpage"
-          style={{
-            backgroundColor: 'white',
-            borderRadius: '50%',
-            width: '36px',
-            height: '36px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          >
-          <InfoOutlinedIcon fontSize="large"  htmlColor="#E3242B"/>
-          </IconButton>
-          </Toolbar>
-          </AppBar>
-          </Grid>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+  return (
+    <div className={classes.root}>
+      <Router>
+        <div className="App">
+          <header>
+            <Grid container sx={{ flexGrow: 1 }}>
+              <AppBar
+                position="fixed"
+                style={{
+                  background: "transparent",
+                  boxShadow: "none",
+                }}
+              >
+                <Toolbar>
+                  <IconButton
+                    aria-label="add to shopping cart"
+                    edge="start"
+                    href="/"
+                    style={{
+                      backgroundColor: "white",
+                      borderRadius: "50%",
+                      width: "36px",
+                      height: "36px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <CatchingPokemonTwoToneIcon
+                      fontSize="large"
+                      htmlColor="#E3242B"
+                    />
+                  </IconButton>
+                  <div style={{ flexGrow: 1 }}></div>
+                  <IconButton
+                    aria-label="add to shopping cart"
+                    edge="start"
+                    href="/otherpage"
+                    style={{
+                      backgroundColor: "white",
+                      borderRadius: "50%",
+                      width: "36px",
+                      height: "36px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <InfoOutlinedIcon fontSize="large" htmlColor="#E3242B" />
+                  </IconButton>
+                </Toolbar>
+              </AppBar>
+            </Grid>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+          <Copyright />
         </div>
-        <Copyright/>
-      </div>
-    </Router>
+      </Router>
     </div>
   );
 }
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect,useMemo} from 'react';
-import axios from 'axios';
-import {Box,Chip, IconButton,TextField,InputAdornment,Paper,Card} from '@mui/material';
-import MaterialReactTable from 'material-react-table';
-import { BrowserRouter as  Link } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import './Fib.css';
-import SearchSharpIcon from '@mui/icons-material/SearchSharp';
-import Trans from './Trans.jsx';
-import CatchingPokemonTwoToneIcon from '@mui/icons-material/CatchingPokemonTwoTone';
-
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+import {
+  Box,
+  Chip,
+  IconButton,
+  TextField,
+  InputAdornment,
+  Paper,
+} from "@mui/material";
+import MaterialReactTable from "material-react-table";
+import "./Fib.css";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import Trans from "./Trans.jsx";
+import CatchingPokemonTwoToneIcon from "@mui/icons-material/CatchingPokemonTwoTone";
 
 function Fib() {
   const [seenIndexes, setSeenIndexes] = useState([]);
   const [values, setvalues] = useState({});
-  const [index, setIndex] = useState('');
- 
-  const handleChange = (event) => {
-    setIndex(event);
-  };
+  const [index, setIndex] = useState("");
+
+  
   // To add an item:  putAdornment i
-/* setIndex(prevItems => [
+  /* setIndex(prevItems => [
   ...prevItems,
   {  text: newText, offset: 100 }
 ]);
@@ -34,38 +36,28 @@ function Fib() {
       </form>
 *********
 ); */
-const SearchBar = ({index}) => {
-  const BarStyle = {width:"20rem",background:"#F0F0F0", border:"none", padding:"0.5rem"};
-  return (
-    <input
-      style={BarStyle}
-      key="search-bar"
-      value={index}
-      placeholder={"search news"}
-      onChange={(event) => setIndex(event.target.value)}
-     />
-  );
-}
+
 
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'key', //access nested data with dot notation
-        header: 'item',
+        accessorKey: "key", //access nested data with dot notation
+        header: "item",
       },
       {
-        accessorKey: 'link',
-        header: 'link',
+        accessorKey: "link",
+        header: "link",
       },
     ],
-    [],);
+    []
+  );
   useEffect(() => {
     const fetchvalues = async () => {
-      const values = await axios.get('/api/values/current');
+      const values = await axios.get("/api/values/current");
       setvalues(values.data);
     };
     const fetchIndexes = async () => {
-      const seenIndexes = await axios.get('/api/values/all');
+      const seenIndexes = await axios.get("/api/values/all");
       setSeenIndexes(seenIndexes.data);
     };
     try {
@@ -77,34 +69,32 @@ const SearchBar = ({index}) => {
   }, []);
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if(index.trim()!==""){
-    await axios.post('/api/values', {
-      index: index.toLowerCase(),
-    });
-    setIndex('');
-    try {
-      const seenIndexes = await axios.get('/api/values/all');
-      setSeenIndexes(seenIndexes.data);
-//      console.log("index  "+seenIndexes.data)
-      const values = await axios.get('/api/values/current');
-      setvalues(values.data);
-      console.log("values "+JSON.stringify(values.data))
-    } catch (error) {
-      console.log(error);
-    }}
+    if (index.trim() !== "") {
+      await axios.post("/api/values", {
+        index: index.toLowerCase(),
+      });
+      setIndex("");
+      try {
+        const seenIndexes = await axios.get("/api/values/all");
+        setSeenIndexes(seenIndexes.data);
+        const values = await axios.get("/api/values/current");
+        setvalues(values.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
   const RenderSeenIndexes = React.memo(() => {
-    console.log('rendering SeenIndexes');
     const ind = [];
     for (let key in values) {
       ind.push(key);
     }
     ind.sort();
-    return (<Box display="flex"
-    justifyContent="center"
-    alignItems="center">
-    <Trans data={ind}/>
-    </Box>)
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Trans data={ind} />
+      </Box>
+    );
   });
 
   const Rendervalues = React.memo(() => {
@@ -112,93 +102,106 @@ const SearchBar = ({index}) => {
     for (let key in values) {
       arr.push({ key: key, link: values[key] });
     }
-      arr.sort((a,b) => (a.key > b.key) ? 1 : ((b.key > a.key) ? -1 : 0))
-
+    arr.sort((a, b) => (a.key > b.key ? 1 : b.key > a.key ? -1 : 0));
     return (
-        <Box
+      <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
         minHeight="100vh"
-        >
+      >
         <MaterialReactTable columns={columns} data={arr} />
-        </Box>
-      );
-    });
-  const BarStyle = {width:"20rem",background:"#F0F0F0", border:"none",};
+      </Box>
+    );
+  });
+  const BarStyle = { width: "20rem", background: "#F0F0F0", border: "none" };
 
   return (
     <div>
-  
-         <form onSubmit={handleSubmit}>
-<TextField
-      style={BarStyle}
-      key="search-bar"
-      value={index}
-      size="small"
-      variant="standard"
-      label="Pokemon"
-      color='grey'
-      onChange={(event) => setIndex(event.target.value)}
-      padding='5px 0px'
-      InputProps={{
-        style: {
-          padding:0
-        }  ,
-        endAdornment: (
-          <InputAdornment>
-            <IconButton size='large' onClick={handleSubmit} sx={{ "&:hover": { backgroundColor: "transparent" } }}>
-              <SearchSharpIcon fontSize='large'/>
-            </IconButton>
-          </InputAdornment>
-        )
-      }}
-     />
-     </form>
-     <Box
-  display="flex"
-  justifyContent="center"
-  alignItems="center"
-  left='50%'
-        top= '50%'
->
-      <Paper sx={{mt:3, pb:2, width:"600px", }}>
-      <Chip icon={<CatchingPokemonTwoToneIcon fontSize="medium"  htmlColor="#E3242B" />} 
-      label="Pokemons found till now" 
-      variant="outlined" 
-      style={{backgroundColor:'white'}} 
-      color="error"
-      sx={{
-        m:2,
-        "& .MuiChip-label": {
-          fontSize: 18,
-          fontFamily: "futura",
-          fontWeight: "bold",
-          color: "black"
-        }
-      }}
-      size="large"/>
-      <RenderSeenIndexes />
-      </Paper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          style={BarStyle}
+          sx={{ mt: "35px" }}
+          key="search-bar"
+          value={index}
+          size="small"
+          variant="standard"
+          label="Pokemon"
+          color="grey"
+          onChange={(event) => setIndex(event.target.value)}
+          InputProps={{
+            style: {
+              padding: 0,
+            },
+            endAdornment: (
+              <InputAdornment>
+                <IconButton
+                  size="large"
+                  onClick={handleSubmit}
+                  sx={{ "&:hover": { backgroundColor: "transparent" } }}
+                >
+                  <SearchSharpIcon fontSize="large" />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </form>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        left="50%"
+        top="50%"
+      >
+        <Paper sx={{ mt: 3, pb: 2, width: "600px" }}>
+          <Chip
+            icon={
+              <CatchingPokemonTwoToneIcon
+                fontSize="medium"
+                htmlColor="#E3242B"
+              />
+            }
+            label="Pokemons found till now"
+            variant="outlined"
+            style={{ backgroundColor: "white" }}
+            color="error"
+            sx={{
+              m: 2,
+              "& .MuiChip-label": {
+                fontSize: 18,
+                fontFamily: "futura",
+                fontWeight: "bold",
+                color: "black",
+              },
+            }}
+            size="large"
+          />
+          <RenderSeenIndexes />
+        </Paper>
       </Box>
 
-      <Chip icon={<CatchingPokemonTwoToneIcon fontSize="medium"  htmlColor="#E3242B" />} 
-      label="Your Pokedex" 
-      variant="outlined" 
-      style={{backgroundColor:'white'}} 
-      color="error"
-      sx={{
-        m:2,
-        "& .MuiChip-label": {
-          fontSize: 18,
-          fontFamily: "futura",
-          fontWeight: "bold",
-          color: "black"
+      <Chip
+        icon={
+          <CatchingPokemonTwoToneIcon fontSize="medium" htmlColor="#E3242B" />
         }
-      }}
-      size="large"/>
+        label="Your Pokedex"
+        variant="outlined"
+        style={{ backgroundColor: "white" }}
+        color="error"
+        sx={{
+          m: 2,
+          "& .MuiChip-label": {
+            fontSize: 18,
+            fontFamily: "futura",
+            fontWeight: "bold",
+            color: "black",
+          },
+        }}
+        size="large"
+      />
       <Rendervalues />
-      </div>
+    </div>
   );
 }
 export default Fib;
