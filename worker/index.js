3d85d2ee79ef63dcf15c69b91c37207623b7054d
EaswarDSharma <easwardsharma@gmail.com>
2023-09-18 19:42:20 +0530

    bobby v1

--- worker/index.js
+++ worker/index.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const keys = require('./keys');
 const redis = require('redis');
 
@@ -14,62 +13,8 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2);
   
 }
-async function edfood(query){
-  url="temporary";
-   await axios.get(`https://api.edamam.com/search?q=${query}&app_id=ac3ed5c4&app_key=32175cda1f892ab16589951f6d022100&from=0&to=1`)
-  .then((response) => {
-    url=response.data.hits[0].recipe.url;
-  })
-  .catch((error) => {
-  url="not found"});
-  return url;
- }
- function getAbilityString(abilities) {
-  const abilityNames = abilities.map((ability) => ability.ability.name);
-  
-  if (abilityNames.length === 1) {
-    return `The ability is ${abilityNames[0]}.`;
-  } else {
-    const lastTwoAbilities = abilityNames.slice(-2).join(" and ");
-    return `The abilities are ${abilityNames.slice(0, -2)} ${lastTwoAbilities}.`;
-  }
-}
-function getStatsString(stats) {
-  const statNames = {
-    hp: "hp",
-    attack: " agility",
-    speed: " atk",
-  };
-
-  const statValues = stats.reduce((result, stat) => {
-    if (stat.stat.name in statNames) {
-      result[stat.stat.name] = stat.base_stat;
-    }
-    return result;
-  }, {});
-
-  const formattedStats = Object.entries(statValues).map(([name, value]) => `${statNames[name]}: ${value}`);
-  return ` Stats are as ${formattedStats}.`;
-}
-
- async function poke(query){
-  url="temporary";
- 
-   await axios.get(`https://pokeapi.co/api/v2/pokemon/${query}`)
-  .then((response) => {
-    url=response.data;
-  })
-  .catch((error) => {
-  url="not found"});
-  return url;
- }
 
 sub.on('message', async (channel, message) => {
-  //redisClient.hset('values', message,await fibb(parseInt(message))
-  const foodmess=await edfood(message)
-  const pokemon= await poke(message)
-  console.log(channel)
-  string=getAbilityString(pokemon.abilities)+getStatsString(pokemon.stats)
-  await redisClient.hset("values", message,string)
+  await redisClient.hset("values", fib(message))
 })
 sub.subscribe('insert');
